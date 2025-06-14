20e99f78c67a319f07fb36ecb4df21e90f3bd399
Fariska Ratna <fariskafauziah07@students.unnes.ac.id>
2025-06-14 11:27:40 +0700

    delete for category

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -176,4 +176,15 @@ class AdminController extends Controller
         $category->save();
         return redirect()->route('admin.categories')->with('status', 'Category has been updated!');
     }
+
+    public function category_delete($id)
+    {
+        $category = Category::find($id);
+        if(File::exists(public_path('uploads/categories').'/'.$category->image))
+        {
+            File::delete(public_path('uploads/categories').'/'.$category->image);
+        }
+        $category->delete();
+        return redirect()->route('admin.categories')->with('status', 'Category has been deleted!');
+    }
 }
